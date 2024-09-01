057b801c5fbe279bf4ba651007e8536c5024137c
Wai Loc <111039553+wailoc25@users.noreply.github.com>
2024-09-01 13:56:36 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.ckm27qy9qrhy.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "leongwl-employee"
 customregion = "us-east-1"
 
